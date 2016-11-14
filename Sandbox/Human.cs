47a8d8ee40354535b43eaed4b48e792ffc35974e
Martin Kierkegaard <mark@easj.dk>
2016-11-14 21:48:57 +0100

    Add project files.

---
+++ Sandbox/Human.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Human
+    {
+        private bool maleOrFemale;
+        private String eyeColor;
+        private String hairColor;
+        private int heightCategory;
+
+        public Human(bool maleOrFemale, String eyeColor, String hairColor, int heightCategory)
+        {
+            this.maleOrFemale = maleOrFemale;
+            this.eyeColor = eyeColor;
+            this.hairColor = hairColor;
+            this.heightCategory = heightCategory;
+        }
+
+        public String GetDescription()
+        {
+            String description = "You got a " + GetGenderDescription();
+            description = description + ", with " + eyeColor + " eyes";
+            description = description + ", " + hairColor + " hair";
+            description = description + ", who is " + GetHeightDescription();
+
+            return description;
+        }
+
+        public String GetGenderDescription()
+        {
+            if (maleOrFemale)
+            {
+                return "man";
+            }
+            else
+            {
+                return "woman";
+            }
+        }
+
+        public String GetHeightDescription()
+        {
+            switch (heightCategory)
+            {
+                case 0:
+                    return "Very short";
+                case 1:
+                    return "Short";
+                case 2:
+                    return "Medium height";
+                case 3:
+                    return "Tall";
+                case 4:
+                    return "Very tall";
+                default:
+                    return "Unknown height";
+            }
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Human h1 = new Human(true, "Brown", "Black", 1);
+            Human h2 = new Human(false, "White", "Blue", 4);
+            Human h3 = new Human(true, "Blond", "Green", 8);
+
+            Console.WriteLine(h1.GetDescription());
+            Console.WriteLine(h2.GetDescription());
+            Console.WriteLine(h3.GetDescription());
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
