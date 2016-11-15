a1e30f3cf4a0547ac4dd37366c716d27242b070d
Rolf Petersen <rollep7@msn.com>
2016-11-15 13:25:49 +0100

    enums og udskrift fra List

--- Sandbox/Human.cs
+++ Sandbox/Human.cs
@@ -7,12 +7,22 @@ namespace Sandbox
 {
     class Human
     {
-        private bool maleOrFemale;
-        private String eyeColor;
-        private String hairColor;
-        private int heightCategory;
+        public enum Køn { man, woman}
+        public enum øjenFarve { rød, grøn, gul, brun, blå}
+        
+        public enum hårfarve { hvid, sort, grøn, rød, blå, gul, grå}
+        public enum Højde { VeryShort, Short, MediumHeight, Tall, VaryTall, UnknownHeight }
 
-        public Human(bool maleOrFemale, String eyeColor, String hairColor, int heightCategory)
+        private Køn maleOrFemale;
+        private øjenFarve eyeColor;
+        private hårfarve hairColor;
+        private Højde heightCategory;
+        //private bool maleOrFemale;
+        //private String eyeColor;
+        //private String hairColor;
+        //private int heightCategory;
+
+        public Human(Køn maleOrFemale, øjenFarve eyeColor, hårfarve hairColor, Højde heightCategory)
         {
             this.maleOrFemale = maleOrFemale;
             this.eyeColor = eyeColor;
@@ -22,43 +32,43 @@ namespace Sandbox
 
         public String GetDescription()
         {
-            String description = "You got a " + GetGenderDescription();
+            String description = "You got a " + maleOrFemale;
             description = description + ", with " + eyeColor + " eyes";
             description = description + ", " + hairColor + " hair";
-            description = description + ", who is " + GetHeightDescription();
+            description = description + ", who is " + heightCategory;
 
             return description;
         }
 
-        public String GetGenderDescription()
-        {
-            if (maleOrFemale)
-            {
-                return "man";
-            }
-            else
-            {
-                return "woman";
-            }
-        }
+        //public String GetGenderDescription()
+        //{
+        //    if (Køn)
+        //    {
+        //        return "man";
+        //    }
+        //    else
+        //    {
+        //        return "woman";
+        //    }
+        //}
 
-        public String GetHeightDescription()
-        {
-            switch (heightCategory)
-            {
-                case 0:
-                    return "Very short";
-                case 1:
-                    return "Short";
-                case 2:
-                    return "Medium height";
-                case 3:
-                    return "Tall";
-                case 4:
-                    return "Very tall";
-                default:
-                    return "Unknown height";
-            }
-        }
+        //public String GetHeightDescription()
+        //{
+        //    switch (heightCategory)
+        //    {
+        //        case 0:
+        //            return "Very short";
+        //        case 1:
+        //            return "Short";
+        //        case 2:
+        //            return "Medium height";
+        //        case 3:
+        //            return "Tall";
+        //        case 4:
+        //            return "Very tall";
+        //        default:
+        //            return "Unknown height";
+        //    }
+        //}
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sandbox
 {
@@ -8,10 +9,38 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
-            Human h1 = new Human(true, "Brown", "Black", 1);
-            Human h2 = new Human(false, "White", "Blue", 4);
-            Human h3 = new Human(true, "Blond", "Green", 8);
+            Human h1 = new Human(Human.Køn.man, Human.øjenFarve.gul,Human.hårfarve.gul ,Human.Højde.MediumHeight );
+            Human h2 = new Human(Human.Køn.woman, Human.øjenFarve.grøn, Human.hårfarve.rød, Human.Højde.Tall);
+            Human h3 = new Human(Human.Køn.man, Human.øjenFarve.rød, Human.hårfarve.hvid, Human.Højde.UnknownHeight );
+            Human h4 = new Human(Human.Køn.man, Human.øjenFarve.rød, Human.hårfarve.hvid, Human.Højde.UnknownHeight);
 
+            if (h3 == h4)
+            {
+                Console.WriteLine("Person h3 og h4 er ens");
+            }
+            else
+            {
+                Console.WriteLine("De er ikke ens");
+            }
+            
+            //Human h1 = new Human(true, "Brown", "Black", 1);
+            //Human h2 = new Human(false, "White", "Blue", 4);
+            //Human h3 = new Human(true, "Blond", "Green", 8);
+            List<Human> mineFolk = new List<Human>();
+            mineFolk.Add(h1);
+            mineFolk.Add(h2);
+            mineFolk.Add(h3);
+
+            
+            Console.WriteLine("En Liste med personer fra enum");
+            foreach (Human p in mineFolk)
+            {
+                Console.WriteLine(p.GetDescription());
+            }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("3 console udskrivninger");
             Console.WriteLine(h1.GetDescription());
             Console.WriteLine(h2.GetDescription());
             Console.WriteLine(h3.GetDescription());
